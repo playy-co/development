89e847e2a308e2d330b799fb240b617748eb6e7e
AkashReddy-playy <mail@akashreddy.in>
2021-05-04 15:26:01 +0530

    Initial commit

---
+++ sanityStudio/src/base.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+import 'firebase/storage';
+export const app = firebase.initializeApp({
+  projectId: 'frb-playy-co',
+  appId: '1:885627607848:web:f8b3289702ac63e97d8e1d',
+  storageBucket: 'frb-playy-co.appspot.com',
+  locationId: 'asia-south1',
+  apiKey: 'AIzaSyA0KqSmq8-XSZgHIpd9OoJb4REzaBdFvYg',
+  authDomain: 'frb-playy-co.firebaseapp.com',
+  messagingSenderId: '885627607848',
+});
