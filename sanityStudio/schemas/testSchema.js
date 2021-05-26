ce52d2a08facd214b9614970b8659f9bd624d7b8
Henrique Doro <opensource@hdoro.dev>
2021-05-26 11:34:55 -0300

    Fixing sanity-plugin-firebase-dam versions

--- sanityStudio/schemas/testSchema.js
+++ sanityStudio/schemas/testSchema.js
@@ -1,42 +1,50 @@
 export default {
-  name: 'testSchema',
-  title: 'testSchema',
-  type: 'document',
+  name: "testSchema",
+  title: "testSchema",
+  type: "document",
   fields: [
     {
-      name: 'video',
-      title: 'Video',
-      type: 'firebase.media',
+      name: "video",
+      title: "Video",
+      type: "firebase.media",
       options: {
-        accept: 'video/*',
-        storeOriginalFilename: true
-      }
+        accept: "video/*",
+        storeOriginalFilename: true,
+      },
     },
     {
-      name: 'audio',
-      title: 'audio',
-      description: 'Doesn\'t store filename',
-      type: 'firebase.media',
+      name: "audio",
+      title: "audio",
+      description:
+        "Doesn't store filename when uploading. Has a custom validation function that will error out to test validation.",
+      type: "firebase.media",
       options: {
-        accept: 'audio/*',
-        storeOriginalFilename: false
-      }
+        accept: "audio/*",
+        storeOriginalFilename: false,
+      },
+      validation: (Rule) =>
+        Rule.custom((value) => {
+          if (value) {
+            return "Invalid!";
+          }
+          return true;
+        }).warning(),
     },
     {
-      name: 'mp4',
-      title: 'mp4',
-      type: 'firebase.media',
+      name: "mp4",
+      title: "mp4",
+      type: "firebase.media",
       options: {
-        accept: '.mp4',
-      }
+        accept: ".mp4",
+      },
     },
     {
-      name: 'm4a',
-      title: 'm4a',
-      type: 'firebase.media',
+      name: "m4a",
+      title: "m4a",
+      type: "firebase.media",
       options: {
-        accept: '.m4a',
-      }
+        accept: ".m4a",
+      },
     },
   ],
-}
+};
